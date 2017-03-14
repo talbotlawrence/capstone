9bf237333ddd880f6f9cc2aa40be06fb3a6cd3d9
Talbot <talbotlawrence75@gmail.com>
2017-03-14 14:41:53 -0500

    setting up angular with browserify with my capstone

---
+++ app/controllers/DownloadingCtrl.js
@@ -0,0 +1,38 @@
+// "use strict";
+
+// //lib/node_modules/ssh2--download--this is working/////////////////////////////////////
+
+// app.controller("DownloadingCtrl", function($scope){
+
+
+//   $scope.downloading = function(){
+//       var Client = require('../../lib/node_modules/ssh2').Client;
+
+//       var connSettings = {
+//            host: 'test.rebex.net',
+//            port: 22, 
+//            username: 'demo',
+//            password: 'password'
+//       };
+
+//       var conn = new Client();
+//       conn.on('ready', function() {
+//           conn.sftp(function(err, sftp) {
+//               if (err) throw err;
+              
+//               var moveFrom = "/readme.txt";
+//               var moveTo = "/home/talbot/workspace/capstone/sftp/readme.txt";
+
+//               sftp.fastGet(moveFrom, moveTo , {}, function(downloadError){
+//                   if(downloadError) throw downloadError;
+
+//                   console.log("Succesfully uploaded");
+//               });
+//           });
+//       }).connect(connSettings);
+//   };
+
+
+// });
+
+// ///////////////////////////////////////////////////////////////////////////////
